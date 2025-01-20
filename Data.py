c001a72aefab9d27ae4d7d29631fa1f4fdc3552e
Hari Prasad <100824079+hari200321@users.noreply.github.com>
2025-01-20 12:22:13 +0530

    Here i've added the all files for your reference

---
+++ Data.py
@@ -0,0 +1,17 @@
+"""
+Data.py
+Which contains all the Data needed for the program to automate
+"""
+
+class Webdata:
+
+    URL = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    DASHBOARD_URL = "https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index"
+    EXCEL_FILE = "D:\\Automation_practise\\Mini_project_2\\DDF_SHEET.xlsx"
+    SHEET_NUMBER = "Sheet1"
+    USERNAME = "Admin"
+    PASSWORD = "admin123"
+    EMPLOYEE_NAME = "test"
+    USERNAME2 = "Styles"
+    PASSWORD2 = "Hippo$090912"
+    CONFIRM_PASSWORD = "Hippo$090912"
